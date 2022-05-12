339ba7a4a754b4e17cf1f8790aeb274fea1d219e
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2022-05-12 19:42:01 +0700

    I hope it finale

---
+++ app.py
@@ -0,0 +1,83 @@
+# from crypt import methods
+import flask
+
+# Editing app.py to load the model
+import numpy as np
+import pickle
+from datetime import datetime
+
+
+# Untuk nyari template
+app = flask.Flask(__name__, template_folder='templates')
+
+# Columns
+columns = ['Location', 'MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine',
+       'WindGustSpeed', 'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am',
+       'Humidity3pm', 'Pressure9am', 'Pressure3pm', 'Cloud9am', 'Cloud3pm',
+       'Temp3pm', 'RainToday', 'year', 'month', 'day']
+
+places = ['Adelaide', 'Albany', 'Albury', 'AliceSprings', 'BadgerysCreek', 'Ballarat', 'Bendigo', 'Brisbane', 'Cairns', 'Canberra', 'Cobar', 'CoffsHarbour', 'Dartmoor', 'Darwin', 'GoldCoast', 'Hobart', 'Katherine', 'Launceston', 'Melbourne', 'MelbourneAirport', 'Mildura', 'Moree', 'MountGambier', 'MountGinini', 'Newcastle', 'Nhil', 'NorahHead', 'NorfolkIsland', 'Nuriootpa', 'PearceRAAF', 'Penrith', 'Perth', 'PerthAirport', 'Portland', 'Richmond', 'Sale', 'SalmonGums', 'Sydney', 'SydneyAirport', 'Townsville', 'Tuggeranong', 'Uluru', 'WaggaWagga', 'Walpole', 'Watsonia', 'Williamtown', 'Witchcliffe', 'Wollongong', 'Woomera']
+
+# Rute
+@app.route('/')
+def main():
+  return(flask.render_template('main.html', len = len(columns), columns = columns, float_features = columns, algorithm='algorithm', places = places, lenplaces = len(places), place="Location"))
+if __name__ == '__main__':
+  app.run(debug=True)
+
+logress = pickle.load(open('./model/logistic_regression.pkl', 'rb'))
+svm = pickle.load(open('./model/svm.pkl', 'rb'))
+
+# Redirecting the API to predict the result 
+@app.route('/predict',methods=['POST'])
+def predict():
+  '''
+  For rendering results on HTML GUI
+  '''
+  name_predict = ['Logistic Regression', 'SVM']
+  request = [x for x in flask.request.form.values()]
+  just_date = datetime.strptime(request.pop(), "%Y-%m-%d")
+  
+  try:
+    float_features = [float(x) for x in request]
+    in_predict = int(float_features.pop(0))
+
+    # Mengubah inputan date jadi year month day
+
+    float_features.append(just_date.year)
+    float_features.append(just_date.month)
+    float_features.append(just_date.day)
+  except:
+    float_features = columns
+    return flask.render_template('main.html',
+      alert_message='Inputnya harus angka', 
+      float_features = columns,
+      len = len(columns),
+      columns = columns,
+      algorithm='algorithm',
+      places = places,
+      lenplaces = len(places),
+      place="Location"
+    )
+
+  final_features = [np.array(float_features)]
+
+  # Memilih prediksi yang digunakan
+  prediction = 0
+  if(in_predict == 0):
+    prediction = logress.predict(final_features)
+  else:
+    prediction = svm.predict(final_features)
+
+
+
+  return flask.render_template('main.html',
+    prediction_text=prediction[0],
+    float_features=float_features,
+    len = len(columns),
+    columns = columns,
+    algorithm = name_predict[in_predict],
+    places = places,
+    lenplaces = len(places),
+    place=places[int(float_features[0])]
+  )
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Deploy Model
+
+## Deploy to Heroku
+
+1. Pertama login dulu, ini hanya sekali saja
+2. ketik heroku create nama-model
+3. Buat file requirements.txt yg berisi bahan untuk diinstall
+4. procfile, ini ketik aja web:gunicorn app:app
+5. Set remote dengan heroku git:remote -a nama-model
+6. git push heroku master atau git push heroku main
